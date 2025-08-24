b5c80ada64628a333b1c71e266af54adc46cdca0
Ryu <136865700+k23023-university-student@users.noreply.github.com>
2025-08-24 18:13:24 +0900

    Update: メインロジックの実装(獲物の生成時不具合あり)

--- Assets/Scenes/MainScene/MainGameSystem.cs
+++ Assets/Scenes/MainScene/MainGameSystem.cs
@@ -1,29 +1,56 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+
 public class MainGameSystem : MonoBehaviour
 {
     public TMP_Text scoreText;
+    public GameObject boidController;
+    public GameObject explosion;
     private bool isPressed = false;
     private Vector3 originalPosition;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // BoidControllerの移動速度（Unityインスペクターから指定可能）
+    [Header("BoidControllerの移動速度（z軸負方向）")]
+    public float boidControllerMoveSpeed = 10f;
+
+    // 生成されたBoidControllerのリスト
+    private List<GameObject> spawnedBoidControllers = new List<GameObject>();
+
     void Start()
     {
         PointMemory.point = 0;
-        
         originalPosition = transform.localPosition;
-        
+
+        // コルーチンでBoidControllerの自動スポーンを開始
+        StartCoroutine(SpawnBoidControllerCoroutine());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // シフトキーが押されたらシーンを切り替える
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // 手動スポーン（デバッグ用、必要なら残す）
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Vector3 spawnPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 30f);
+            GameObject obj = Instantiate(boidController, spawnPos, transform.rotation);
+            spawnedBoidControllers.Add(obj);
+        }
+
+        // スペースキーが押されたらシーンを切り替える
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("FinishScene");
         }
+
         scoreText.text = "Score: " + PointMemory.point.ToString();
+
         // リターンキーを押している間だけローカル座標を基準からz軸方向に10だけ動かす
         // リターンキーを押している間、滑らかにz軸方向に5だけ移動し、離したら元の位置に滑らかに戻る
         if (Input.GetKey(KeyCode.Return))
@@ -37,7 +64,41 @@ public class MainGameSystem : MonoBehaviour
         float targetZ = isPressed ? 5f : 0f;
         Vector3 targetPosition = originalPosition + new Vector3(0, 0, targetZ);
         transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * 8f);
+
+        // BoidControllerの移動と破棄処理
+        for (int i = spawnedBoidControllers.Count - 1; i >= 0; i--)
+        {
+            GameObject obj = spawnedBoidControllers[i];
+            if (obj == null)
+            {
+                spawnedBoidControllers.RemoveAt(i);
+                continue;
+            }
+            // z軸負方向に移動
+            obj.transform.position += new Vector3(0, 0, -boidControllerMoveSpeed * Time.deltaTime);
+
+            // 自分のz座標を超えたら破棄
+            if (obj.transform.position.z <= transform.position.z)
+            {
+                Destroy(obj);
+                spawnedBoidControllers.RemoveAt(i);
+            }
+        }
     }
+
+    IEnumerator SpawnBoidControllerCoroutine()
+    {
+        while (true)
+        {
+            float waitTime = Random.Range(1f, 5f);
+            yield return new WaitForSeconds(waitTime);
+
+            Vector3 spawnPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 30f);
+            GameObject obj = Instantiate(boidController, spawnPos, transform.rotation);
+            spawnedBoidControllers.Add(obj);
+        }
+    }
+
     void OnTriggerEnter(Collider collision)
     {
         // 衝突したオブジェクトの名前を表示
@@ -45,18 +106,12 @@ public class MainGameSystem : MonoBehaviour
         if (collision.gameObject.name == "Bone" && isPressed)
         {
             GameObject toRemove = collision.gameObject.transform.parent.parent.gameObject;
-            Debug.Log("消去:"+ toRemove.name);
+            // explositionオブジェクトをtoremoveの位置に生成
+            Instantiate(explosion, toRemove.transform.position, Quaternion.identity);
+            Debug.Log("消去:" + toRemove.name);
             PointMemory.point += 1200;
             Destroy(toRemove);
-
-
         }
-        
-        // 衝突情報を取得する例
-        // ContactPoint[] contacts = collision.contacts;
-        // foreach (ContactPoint contact in contacts)
-        // {
-        //     Debug.Log("接触点位置: " + contact.point);
-        // }
+
     }
 }
