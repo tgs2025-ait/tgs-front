5d8ffa68e115ce2e1576367e52ded4aa56302555
Ryu <136865700+k23023-university-student@users.noreply.github.com>
2025-08-29 15:48:38 +0900

    Add: 息継ぎの動きを追加

--- Assets/Scenes/MainScene/move.cs
+++ Assets/Scenes/MainScene/move.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
-
+using System.Collections;
+//移動を制御するスクリプト
 public class Move : MonoBehaviour
 {
+    private bool isThrowing = false;
+    private float throwVelocity = 0f;
+    private float gravity = -20f; // 重力加速度
+    [Header("息継ぎの時に水面から出る勢い(後で変更が必要かも)")]
+    public float throwPower = 10f; // 投げ上げ初速度
+    private float baseY = 0f;   // 現在のジャンプ基準Y
+
+    // 垂直なげあげを開始する関数（基準Yを引数で指定可能）
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,7 +24,8 @@ public class Move : MonoBehaviour
         float horizontal = 0f;
         float vertical = 0f;
         float upDown = 0f;
-
+        if(Input.GetKeyDown(KeyCode.L) && !isThrowing) ThrowUp();
+        HandleThrow();
         if (Input.GetKey(KeyCode.W)) vertical += 1f;
         if (Input.GetKey(KeyCode.S)) vertical -= 1f;
         if (Input.GetKey(KeyCode.A) || SerialReceive.pitchAngle <= -60) horizontal -= 1f;
@@ -27,5 +37,32 @@ public class Move : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, upDown, vertical).normalized;
         transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
         
+    }
+    // 息継ぎ用関数(ジャンプ)
+    void ThrowUp()
+    {
+        isThrowing = true;
+        throwVelocity = throwPower;
+        baseY = transform.position.y;
+    }
+
+    // Update内で呼び出す処理(ジャンプ)
+    void HandleThrow()
+    {
+        Debug.Log("isThrowing: " + isThrowing);
+        if (isThrowing)
+        {
+            throwVelocity += gravity * Time.deltaTime;
+            Vector3 pos = transform.position;
+            pos.y += throwVelocity * Time.deltaTime;
+
+            if (pos.y <= baseY)
+            {
+                pos.y = baseY;
+                isThrowing = false;
+                throwVelocity = 0f;
+            }
+            transform.position = pos;
+        }
     }
 }
